33b547243d84d81f9a71f8bb30ddb39133344dce
kidow <wcgo2ling@gmail.com>
2022-04-19 21:42:46 +0900

    favicon 교체 및 primary color 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,10 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <header className="hero hero--primary">
+      <header className="bg-primary">
         <div className="container mx-auto">
           <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle text-2xl">{siteConfig.tagline}</p>
+          <p className="text-2xl hero__subtitle">{siteConfig.tagline}</p>
           <div className="">
             <Link
               className="button button--secondary button--lg"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#25C19F'
+      }
+    }
   },
   plugins: [require('prettier-plugin-tailwindcss')]
 }
