e9162c71c801bb75e49c54a04e1499789f34dbb2
kidow <wcgo2ling@gmail.com>
2022-04-21 01:46:14 +0900

    기본 템플릿 넣는 중

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
