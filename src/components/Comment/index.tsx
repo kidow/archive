3f063e0ff3b733c03d5c9f9a19b44ae6c4728f96
kidow <wcgo2ling@gmail.com>
2022-04-21 01:06:12 +0900

    giscus 적용

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -9,40 +9,40 @@ const Comment: FC<Props> = () => {
   const theme = isDarkTheme ? 'dark_dimmed' : 'light'
   const ref = useRef<HTMLDivElement>(null)
 
-  const el: HTMLIFrameElement = ref.current.querySelector('iframe.giscus-frame')
-
-  const attributes = {
-    src: 'https://giscus.app/client.js',
-    'data-repo': 'kidow/archive',
-    'data-repo-id': 'R_kgDOHMhSiA',
-    'data-category': 'Show and tell',
-    'data-category-id': 'DIC_kwDOHMhSiM4COpSN',
-    'data-mapping': 'title',
-    'data-reactions-enabled': '1',
-    'data-emit-metadata': '0',
-    'data-theme': theme,
-    'data-lang': 'ko',
-    crossOrigin: 'anonymous',
-    async: 'true'
-  }
-
-  const createGiscus = () => {
-    const script = document.createElement('script')
-    Object.entries(attributes).forEach(([key, value]) =>
-      script.setAttribute(key, value)
-    )
-    ref.current?.appendChild(script)
-  }
-
-  const postThemeMessage = () => {
-    el.contentWindow.postMessage(
-      { giscus: { setConfig: { theme } } },
-      'https://giscus.app'
-    )
-  }
-
   useEffect(() => {
-    el ? postThemeMessage() : createGiscus()
+    const giscusEl: HTMLIFrameElement = ref.current.querySelector(
+      'iframe.giscus-frame'
+    )
+    const createGiscus = () => {
+      const script = document.createElement('script')
+      const attributes = {
+        src: 'https://giscus.app/client.js',
+        'data-repo': 'kidow/archive',
+        'data-repo-id': 'R_kgDOHMhSiA',
+        'data-category': 'Show and tell',
+        'data-category-id': 'DIC_kwDOHMhSiM4COpSN',
+        'data-mapping': 'title',
+        'data-reactions-enabled': '1',
+        'data-emit-metadata': '0',
+        'data-theme': theme,
+        'data-lang': 'ko',
+        crossOrigin: 'anonymous',
+        async: 'true'
+      }
+      Object.entries(attributes).forEach(([key, value]) =>
+        script.setAttribute(key, value)
+      )
+      ref.current?.appendChild(script)
+    }
+
+    const postThemeMessage = () => {
+      giscusEl.contentWindow.postMessage(
+        { giscus: { setConfig: { theme } } },
+        'https://giscus.app'
+      )
+    }
+
+    giscusEl ? postThemeMessage() : createGiscus()
   }, [theme])
   return <div ref={ref} />
 }
