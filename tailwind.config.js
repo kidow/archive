5f02c2a79d6c88317159fe596e3a515e4742bc68
kidow <wcgo2ling@gmail.com>
2022-06-13 21:53:45 +0900

    install tailwindcss latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const plugin = require('tailwindcss/plugin')
 const colors = require('tailwindcss/colors')
 
-/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
