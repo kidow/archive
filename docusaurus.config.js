198e2e7f716437be628c2c34c3226c375bf62d14
kidow <wcgo2ling@gmail.com>
2022-07-30 13:58:15 +0900

    beta cdn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
     {
       type: 'text/javascript',
       async: true,
-      src: 'https://cdn.kidow.me/plugin.js',
+      src: 'https://beta.cdn.feedbank.app/plugin.js',
       'plugin-key': 'b4164b86-acd0-4f10-8171-906046cd71ab'
     }
   ],
