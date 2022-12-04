d11d33a33ddd200ec4481e60c877dde2b7aa6950
kidow <wcgo2ling@gmail.com>
2022-12-05 01:04:21 +0900

    algolia 적용 완료

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,8 +40,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/kidow/archive',
           remarkPlugins: [
             [require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]
           ]
@@ -52,16 +51,12 @@ const config = {
       })
     ]
   ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
         title: '',
-        logo: {
-          alt: 'Logo',
-          src: 'img/logo.svg'
-        },
+        logo: { alt: 'Logo', src: 'img/logo.svg' },
         items: [
           {
             type: 'doc',
@@ -86,88 +81,34 @@ const config = {
                 label: 'Components',
                 to: '/docs/components/data-display/Avatar'
               },
-              {
-                label: 'Hooks',
-                to: '/docs/hooks/useComponentDidUpdate'
-              },
-              {
-                label: 'Utils',
-                to: '/docs/utils/base64ToFile'
-              },
-              {
-                label: 'Settings',
-                to: '/docs/settings/chrome-extensions'
-              },
-              {
-                label: 'Tips',
-                to: '/docs/tips/모바일에서-localhost-보는-법'
-              }
+              { label: 'Hooks', to: '/docs/hooks/useComponentDidUpdate' },
+              { label: 'Utils', to: '/docs/utils/base64ToFile' },
+              { label: 'Settings', to: '/docs/settings/chrome-extensions' },
+              { label: 'Tips', to: '/docs/tips/모바일에서-localhost-보는-법' }
             ]
           },
           {
             title: 'Projects',
             items: [
-              {
-                label: 'Analytics',
-                href: 'https://kidow.me'
-              },
-              {
-                label: 'Admin',
-                href: 'https://admin.kidow.me'
-              },
-              {
-                label: 'Archive',
-                href: 'https://archive.kidow.me'
-              },
-              {
-                label: 'Blog',
-                href: 'https://blog.kidow.me'
-              },
-              {
-                label: 'Components',
-                href: 'https://components.kidow.me'
-              },
-              {
-                label: 'Log',
-                href: 'https://log.kidow.me'
-              },
-              {
-                label: 'Memo',
-                href: 'https://memo.kidow.me'
-              },
-              {
-                label: 'Menu',
-                href: 'https://menu.kidow.me'
-              },
-              {
-                label: 'Opengraph',
-                href: 'https://opengraph.kidow.me'
-              },
-              {
-                label: 'Portal',
-                href: 'https://portal.kidow.me'
-              },
-              {
-                label: 'Resume',
-                href: 'https://resume.kidow.me'
-              },
-              {
-                label: 'Status',
-                href: 'https://status.kidow.me'
-              }
+              { label: 'Analytics', href: 'https://kidow.me' },
+              { label: 'Admin', href: 'https://admin.kidow.me' },
+              { label: 'Archive', href: 'https://archive.kidow.me' },
+              { label: 'Blog', href: 'https://blog.kidow.me' },
+              { label: 'Components', href: 'https://components.kidow.me' },
+              { label: 'Log', href: 'https://log.kidow.me' },
+              { label: 'Memo', href: 'https://memo.kidow.me' },
+              { label: 'Menu', href: 'https://menu.kidow.me' },
+              { label: 'Opengraph', href: 'https://opengraph.kidow.me' },
+              { label: 'Portal', href: 'https://portal.kidow.me' },
+              { label: 'Resume', href: 'https://resume.kidow.me' },
+              { label: 'Status', href: 'https://status.kidow.me' }
             ]
           },
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                href: 'https://blog.kidow.me'
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/kidow'
-              }
+              { label: 'Blog', href: 'https://blog.kidow.me' },
+              { label: 'GitHub', href: 'https://github.com/kidow' }
             ]
           }
         ],
@@ -185,11 +126,10 @@ const config = {
         {
           name: 'keywords',
           content:
-            'docusaurus, code-archive, javascript, react, tailwindcss, typescript, vercel'
+            'docusaurus, javascript, react, tailwindcss, typescript, vercel'
         },
         { name: 'author', content: '김동욱' },
         { name: 'theme-color', content: '#25C19F' },
-
         {
           name: 'google-site-verification',
           content: 'dpMF3-oHfMYFVkjgJpIJSGM_W_aL_gSFFnmWHM90NHU'
@@ -198,23 +138,15 @@ const config = {
           name: 'naver-site-verification',
           content: 'a1d88c86801e2d91cdbdbcd4f784d39897cd2c2b'
         },
-
         { name: 'og:title', content: title },
-        {
-          name: 'og:description',
-          content: description
-        },
+        { name: 'og:description', content: description },
         { name: 'og:url', content: 'https://archive.kidow.me' },
-        {
-          name: 'og:image',
-          content: image
-        },
+        { name: 'og:image', content: image },
         { name: 'og:image:width', content: '1200' },
         { name: 'og:image:height', content: '600' },
         { name: 'og:locale', content: 'ko_KR' },
         { name: 'og:type', content: 'website' },
         { name: 'og:site_name', content: "Web developer Kidow's Code Archive" },
-
         { name: 'twitter:title', content: title },
         { name: 'twitter:description', content: description },
         { name: 'twitter:image', content: image },
@@ -224,7 +156,8 @@ const config = {
       algolia: {
         appId: 'SOZUQMWDC4',
         apiKey: 'a5d0a9e15439d60758cfac77349b97ec',
-        indexName: 'archive'
+        indexName: 'archive',
+        contextualSearch: true
       }
     })
 }
